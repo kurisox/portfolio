9c43a8be7d28149d82d3dc41371762e074ab423f
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-01-19 20:09:35 +0100

    add Providers component for theming

---
+++ website/src/app/provider.tsx
@@ -0,0 +1,14 @@
+// app/providers.tsx
+'use client'
+import {HeroUIProvider} from '@heroui/react'
+import {ThemeProvider } from "next-themes";
+
+export function Providers({children}: { children: React.ReactNode }) {
+  return (
+    <HeroUIProvider>
+      <ThemeProvider attribute="class" defaultTheme='light'>
+        {children}
+      </ThemeProvider>
+    </HeroUIProvider>
+  )
+}
