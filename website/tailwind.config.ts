063f7ddf81207db29856492bf8443752a1514208
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-01-19 20:54:48 +0100

    update Tailwind CSS configuration to support dark mode and add Heroui plugin for theming

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -1,18 +1,37 @@
 import type { Config } from "tailwindcss";
+import { heroui } from "@heroui/react";
+
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        switchDarkMode: "#95c2ff",
+        switchLightMode: "#f7c631", 
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui({
+    themes: {
+      light: {
+        colors: {
+          background: "#f5f4ee",
+          foreground: "#5a4f4a",
+        },
+      },
+      dark: {
+        colors: {
+          background: "#393835",
+          foreground: "#c4bfbc",
+        },
+      },
+    },
+  }),],
 } satisfies Config;
