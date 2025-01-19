5632857222a15710aa70f3637126909935f1f29e
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-01-19 19:59:17 +0100

    add navbar configuration and content retrieval function

---
+++ website/src/app/content_config/navbar_config.ts
@@ -0,0 +1,16 @@
+export const  homeURL = "/";
+
+const navbar_content = [
+    {
+        title: "LEBENSLAUF",
+        url: "resumee"
+    },
+    {
+        title: "PROJEKTE",
+        url: "projects"
+    },
+];
+
+export function getNavbarContent() {
+    return navbar_content;
+}
