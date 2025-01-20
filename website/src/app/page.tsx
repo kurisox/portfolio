6f5c964ca138be61e5b4d948ecb5e39fb0c18754
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-01-20 13:31:45 +0100

    update Home component layout with light and dark mode styles

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,9 +1,8 @@
-
-
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className="h-screen">
+      <div className="h-1/2 bg-ligghtmodeBG text-lightmodeFont2 dark:bg-darkmodeBG1 dark:text-darkmodeFont">Hero Dummy</div>
+      <div className="h-1/2 bg-lightmodeBG2 text-lightmodeFont2 dark:bg-darkmodeBG1 dark:text-darkmodeFont">Content</div>
     </div>
   );
 }
