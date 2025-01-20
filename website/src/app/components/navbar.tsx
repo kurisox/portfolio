b828c64e6ec32160e567a351cd9ef125c78f6360
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-01-20 13:32:00 +0100

    add ThemeSwitch to NavigationBar for light and dark mode support

--- website/src/app/components/navbar.tsx
+++ website/src/app/components/navbar.tsx
@@ -10,6 +10,7 @@ import {
   NavbarMenu,
   NavbarMenuItem,
 } from "@heroui/react";
+import ThemeSwitch from "./themeSwitch";
 
 export default function NavigationBar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -18,41 +19,41 @@ export default function NavigationBar() {
   return (
     <Navbar
       maxWidth="full"
-      className="light bg-lightmodeBG2"
+      className="bg-lightmodeBG2 dark:bg-darkmodeBG2"
       onMenuOpenChange={setIsMenuOpen}
     >
       <NavbarContent>
         <NavbarBrand>
-          <p className="text-24 font-raleway text-lightmodeFont hover:text-red-800">
+          <p className="text-24 font-raleway text-lightmodeFont1 hover:text-red-800 dark:text-darkmodeFont">
             {author}
           </p>
         </NavbarBrand>
       </NavbarContent>
-      <NavbarContent justify="end" className="md:hidden">
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <ThemeSwitch />
+        </NavbarItem>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="text-lightmodeFont md:hidden"
+          className="text-lightmodeFont1 dark:text-darkmodeFont md:hidden"
         />
-      </NavbarContent>
-      <NavbarContent justify="end" className="hidden md:flex">
         {getNavbarContent().map((item, index) => (
-          <NavbarItem className="" key={index}>
+          <NavbarItem className="hidden md:flex" key={index}>
             <a
               href={item.url}
-              className="text-18 font-raleway text-lightmodeFont hover:text-red-800"
+              className="text-18 font-raleway text-lightmodeFont1 hover:text-red-800 dark:text-darkmodeFont dark:hover:text-red-400"
             >
               {item.title}
             </a>
           </NavbarItem>
         ))}
       </NavbarContent>
-
-      <NavbarMenu className="light bg-lightmodeBG2">
+      <NavbarMenu className="bg-lightmodeBG2 dark:bg-darkmodeBG2 ">
         {getNavbarContent().map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <a
               href={item.url}
-              className="text-18 font-raleway text-lightmodeFont hover:text-red-800"
+              className="text-18 font-raleway text-lightmodeFont1 hover:text-red-800 dark:text-darkmodeFont dark:hover:text-red-400"
             >
               {item.title}
             </a>
