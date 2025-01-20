a4233d1c2080add1264a80dc2251df8de8aba802
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-01-20 13:31:12 +0100

    add NavigationBar component to Providers

--- website/src/app/provider.tsx
+++ website/src/app/provider.tsx
@@ -2,11 +2,13 @@
 'use client'
 import {HeroUIProvider} from '@heroui/react'
 import {ThemeProvider } from "next-themes";
+import NavigationBar from './components/navbar';
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
     <HeroUIProvider>
       <ThemeProvider attribute="class" defaultTheme='light'>
+        <NavigationBar/>
         {children}
       </ThemeProvider>
     </HeroUIProvider>
