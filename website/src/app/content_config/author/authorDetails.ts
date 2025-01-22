69966f52d7b7419571628a91558a6573b31ca08a
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-01-22 19:50:48 +0100

    feat: add author and career log entry configuration files

---
+++ website/src/app/content_config/author/authorDetails.ts
@@ -0,0 +1,18 @@
+import { AuthorConfig } from "./author_config";
+
+export const authorConfig: AuthorConfig = {
+    name: "Jan-Henrik Capsius",
+    jobtitle: "Junior Software Developer",
+    description:
+      "Ich bin Junior Softwareentwickler mit dem Schwerpunkt auf Backend- und DevOps Entwicklung. " +
+      "Ich liebe es mich in neue Technologien oder Entwicklungsmethoden einzulesen und komplexe technische " +
+      "Herausforderungen zu lösen.",
+    interests:
+      "Java, JUnit, Mockito und Spring Boot, Docker, CI/CD, DevOps, Microservices,Clean Code, " +
+      "Test Driven Development und seit neuestem CSharp und .NET Core",
+    hobbies: "Outdoor-Aktivitäten mit meinem Hund, schwimmen und alles rund ums Kochen",
+    voluntarywork: "Umweltschutz auf vier Pfoten und regelmäßiges Blutspenden",
+    email: "capsiusjanhenrik.bewerbung@gmail.com",
+    github: "https://github.com/kurisox",
+    linkedin: "https://linkedin.com/in/janhenrikcapsius",
+  };
---
+++ website/src/app/content_config/career/logentryDetails.ts
@@ -0,0 +1,143 @@
+import { LogEntryProps } from "./logentry_config";
+
+export const logEntryConfig: LogEntryProps[] = [
+    {
+      entry: {
+        added: true,
+        version: "2.1.0",
+        position: "Auszubildender Fachinformatiker für Anwendungsentwicklung",
+        company: "Unternehmensgruppe Brüder Schlau",
+        startDate: "Aug 2022",
+        endDate: "Jan 2025",
+        description: [
+          "Entwicklung und Anpassung von E-Commerce-Funktionen im SAP Hybris Umfeld",
+          "Integration und Schnittstellenmanagement",
+          "Planung und Entwicklung von internen Software-Lösungen",
+          "Planung und Entwicklung von Entwicklungsumgebungen in Docker",
+          "Dokumentation und Wissensvermittlung neuer Software und Prozesse",
+          "Bau und Deployment neuer Online-Shop Releases",
+        ],
+        deprecated: "Arbeitstätigkeit beendet",
+        changed: "",
+      },
+    },
+    {
+      entry: {
+        added: true,
+        version: "0.5.0",
+        position: "Berufsschüler",
+        company: "Berufskolleg Lübbecke",
+        startDate: "Aug 2022",
+        endDate: "Jan 2025",
+        description: [
+          "Einrichten eines IT-gestützten Arbeitsplatzes: 82/100 Punkten",
+          "Planen und Umsetzen eines Softwareprojektes: 94/100 Punkten",
+          "Planen eines Softwareproduktes: 85/100 Punkten",
+          "Entwicklung und Umsetzung von Algorithmen: 86/100 Punkten",
+          "Wirtschafts- und Sozialkunde: 69/100 Punkten",
+          "Berufsschulzeugnis: 1,8",
+        ],
+        deprecated: "",
+        changed: "",
+      },
+    },
+    {
+      entry: {
+        added: true,
+        version: "2.0.0",
+        position: "Studentische Hilfskraft in der Datenverarbeitungzentrale",
+        company: "Hochschule Bielefeld (HSBI)",
+        startDate: "Okt 19",
+        endDate: "Jul 2022",
+        description: [
+          "Auf- und Abbau von Hardware-Systemen",
+          "Überprüfung und Instandhaltung von Hardware-Systemen",
+          "Ausrollen von Software-Projekten",
+          "Automatisierung von Prozessen durch Skripte",
+        ],
+        deprecated: "Arbeitstätigkeit beendet",
+        changed: "Wechsel zu neuem Arbeitgeber",
+      },
+    },
+    {
+      entry: {
+        added: true,
+        version: "0.4.0",
+        position: "Student",
+        company: "Hochschule Bielefeld (HSBI)",
+        startDate: "Okt 2016",
+        endDate: "Jul 2019",
+        description: [
+          "Benutzen von Java und das objektierte Programmieren",
+          "Entwicklung von Algorithmen und Verständnis für Datenstrukturen",
+          "Benutzen von Entwurfsmuster",
+          "Benutzen von Clean Code Prinzipien",
+          "Benutzen von Taktiken bezüglich Testing und Test Driven Developlment mit dem JUnit-Framework",
+          "Benutzen des Mocking-Framework Mockito",
+          "Wissen im Projektmanagement",
+          "Wissen im Software-Engineering",
+          "Benutzen von Node.js, React und MYSQL für Webentwicklung",
+        ],
+        deprecated: "Ohne Abschluss",
+        changed:
+          "Wechsel in die Ausbildung zum Fachinformatiker für Anwendungsentwicklung",
+      },
+    },
+    {
+      entry: {
+        added: true,
+        version: "1.0.0",
+        position: "Bauzeichner im Hochbau",
+        company: "IGS ING.-BÜRO BAUWESEN",
+        startDate: "Okt 16",
+        endDate: "Jul 2019",
+        description: [
+          "Anfertigung von Bauantragsformularen",
+          "Anfertigung von Grundrissen, Ansichten und Schnitte für Bauanträge",
+          "Anfertigung Detailpläne",
+        ],
+        deprecated: "Arbeitstätigkeit beendet",
+        changed: "Wechsel zu neuem Arbeitgeber",
+      },
+    },
+    {
+      entry: {
+        added: true,
+        version: "0.3.0",
+        position: "Student",
+        company: "Leibniz University Hannover",
+        startDate: "Okt 2016",
+        endDate: "Jul 2019",
+        description: ["Bau- und Umweltingenieurwesen", "(Bachelor of Science)"],
+        deprecated: "Ohne Abschluss",
+        changed: "Wechsel in das Informatikstudium",
+      },
+    },
+    {
+      entry: {
+        added: true,
+        version: "0.2.0",
+        position: "Berufsschüler",
+        company: "Berufsbildende Schulen Neustadt am Rübenberge",
+        startDate: "2015",
+        endDate: "2016",
+        description: ["Fachhochschulreife Technik", "Abschluss: 2,8"],
+        deprecated: "",
+        changed: "",
+      },
+    },
+    {
+      entry: {
+        added: true,
+        version: "0.1.0",
+        position: "Auszubildender Bauzeichner im Hochbau",
+        company: "IGS ING.-BÜRO BAUWESEN",
+        startDate: "Aug 2012",
+        endDate: "Jun 2015",
+        description: ["Abschluss Bauzeichner im Hochbau: 3,0"],
+        deprecated: "",
+        changed: "",
+      },
+    },
+  ];
+  
