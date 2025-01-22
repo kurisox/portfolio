2720a10005d9f5a0836481e8808397b8a7f46a80
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-01-22 18:22:44 +0100

    feat: add changelog configuration for career log details

---
+++ website/src/app/content_config/career/careerlogDetails.ts
@@ -0,0 +1,9 @@
+import { ChangelogConfig } from "./changelog_config";
+
+export const changelogConfig: ChangelogConfig = {
+    disclaimer: "Dieser Lebenslauf ist ein kleines, spaßiges Experiment um meinen " +
+            "beruflichen und schulischen Werdegang in Form eines Changelogs einer " +
+            "Software darzustellen. Für einen vollständigen Überblick über meine " +
+            "Erfahrungen und Fähigkeiten. Schau dir doch gerne meine " +
+            "Lebenslauf-Seite an. Dort findest du auch ein herunterladbares PDF.",
+}
