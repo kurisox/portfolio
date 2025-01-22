4d1eee0d93244a96e16dbbc106a1c576d89a0d3d
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-01-22 19:16:33 +0100

    refactor: improve formatting and readability in careerlog component

--- website/src/app/components/careerlog/careerlog.tsx
+++ website/src/app/components/careerlog/careerlog.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { changelogConfig } from "@/app/content_config/career/careerlogDetails";
 import { logEntryConfig } from "@/app/content_config/career/logentryDetails";
-import {mockLogentry} from "@/app/content_config/career/mockLogentryDetails";
+import { mockLogentry } from "@/app/content_config/career/mockLogentryDetails";
 import LogEntry from "./logentry";
 export default function Carrerlog() {
-  const logEntry = process.env.NEXT_PUBLIC_DEV_MODE === 'true' ? mockLogentry : logEntryConfig;
+  const logEntry =
+    process.env.NEXT_PUBLIC_DEV_MODE === "true" ? mockLogentry : logEntryConfig;
   return (
     <div className="pt-4 width-screen flex flex-row bg-lightmodeBG2 dark:bg-darkmodeBG2">
       <div className="flex flex-col items-center ml-auto mr-auto w-5/6 md:w-2/3 2xl:flex-row 2xl:justify-center 2xl:items-start 2xl:gap-20 2xl:w-1/2">
@@ -12,8 +13,7 @@ export default function Carrerlog() {
           <h1 className="pb-4 text-40 font-raleway text-lightmodeFont2 dark:text-darkmodeFont">
             Career Changelog
           </h1>
-          <div className="pb- pt-4 mb-8 bg-changelog dark:text-lightmodeFont2 flex flex-row justify-center items-center gap-2">
-            <p className="text-64 pl-2">.</p>
+          <div className="p-4 mb-8 bg-changelog dark:text-lightmodeFont2 flex flex-row justify-center items-center gap-2">
             <p className="text-18 text-lightmodeFont2 dark:text-lightmodeFont2">
               {changelogConfig.disclaimer}
             </p>
@@ -24,8 +24,21 @@ export default function Carrerlog() {
           </p>
           <br />
           <p className="text-18 text-lightmodeFont2 dark:text-darkmodeFont">
-            Das Format basiert auf <a className="text-switchLightMode dark:text-switchDarkMode" href="https://keepachangelog.com/de/1.0.0/">Keep a Changelog</a>, und dieses Projekt folgt
-            dem Prinzip der <a className="text-switchLightMode dark:text-switchDarkMode" href="https://semver.org/lang/de/">semantischen Versionierung</a>.
+            Das Format basiert auf{" "}
+            <a
+              className="text-switchLightMode dark:text-switchDarkMode"
+              href="https://keepachangelog.com/de/1.0.0/"
+            >
+              Keep a Changelog
+            </a>
+            , und dieses Projekt folgt dem Prinzip der{" "}
+            <a
+              className="text-switchLightMode dark:text-switchDarkMode"
+              href="https://semver.org/lang/de/"
+            >
+              semantischen Versionierung
+            </a>
+            .
           </p>
           <br />
           {logEntry.map((logEntry, index) => (
