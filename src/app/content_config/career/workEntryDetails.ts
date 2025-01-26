1addea42b7d5e4f1f948c416bc98fddbdc4c91d6
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-01-26 16:53:42 +0100

    renamed

---
+++ src/app/content_config/career/workEntryDetails.ts
@@ -0,0 +1,60 @@
+import { LogEntryProps } from "./logentry_config";
+
+export const logEntryConfig: LogEntryProps[] = [
+    {
+      entry: {
+        added: true,
+        version: "2.1.0",
+        position: "Auszubildender Fachinformatiker Anwendungsentwicklung",
+        company: "Unternehmensgruppe Brüder Schlau",
+        startDate: "Aug 2022",
+        endDate: "Jan 2025",
+        description: [
+          "Entwicklung und Anpassung von E-Commerce-Funktionen im SAP Hybris Umfeld",
+          "Integration und Schnittstellenmanagement",
+          "Planung und Entwicklung von internen Software-Lösungen",
+          "Planung und Entwicklung von Entwicklungsumgebungen in Docker",
+          "Dokumentation und Wissensvermittlung neuer Software und Prozesse",
+          "Bau und Deployment neuer Online-Shop Releases",
+        ],
+        deprecated: "Arbeitstätigkeit beendet",
+        changed: "",
+      },
+    },
+    {
+      entry: {
+        added: true,
+        version: "2.0.0",
+        position: "Studentische Hilfskraft in der Datenverarbeitungzentrale",
+        company: "Hochschule Bielefeld (HSBI)",
+        startDate: "Okt 19",
+        endDate: "Jul 2022",
+        description: [
+          "Auf- und Abbau von Hardware-Systemen",
+          "Überprüfung und Instandhaltung von Hardware-Systemen",
+          "Ausrollen von Software-Projekten",
+          "Automatisierung von Prozessen durch Skripte",
+        ],
+        deprecated: "Arbeitstätigkeit beendet",
+        changed: "Wechsel zu neuem Arbeitgeber",
+      },
+    },
+    {
+      entry: {
+        added: true,
+        version: "1.0.0",
+        position: "Bauzeichner im Hochbau",
+        company: "IGS ING.-BÜRO BAUWESEN",
+        startDate: "Okt 2016",
+        endDate: "Jul 2019",
+        description: [
+          "Anfertigung von Bauantragsformularen",
+          "Anfertigung von Grundrissen, Ansichten und Schnitte für Bauanträge",
+          "Anfertigung Detailpläne",
+        ],
+        deprecated: "Arbeitstätigkeit beendet",
+        changed: "Wechsel zu neuem Arbeitgeber",
+      },
+    },
+  ];
+  
