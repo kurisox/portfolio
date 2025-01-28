1734e5fe605ead5e3259dab2cba58ea3b856ae04
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-01-28 11:23:28 +0100

    feat: integrate BentoGridTwoColumns component into the home page layout

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,8 @@
+import { BentoGridTwoColumns } from "@/app/components/bentogrid/bentoGridTwoColumns";
 export default function Home() {
   return (
-    <div >
-        Work in progress
+    <div className="h-full bg-lightmodeBG dark:bg-darkmodeBG1">
+        <BentoGridTwoColumns/>
     </div>
   );
 }
